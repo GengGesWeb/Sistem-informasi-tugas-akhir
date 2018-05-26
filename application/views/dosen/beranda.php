d4932e7825ad61a8a839f0e2b74e691d47aca428
Dimas Fatah Hilla <dimasfatah2@gmail.com>
2018-05-26 23:37:39 +0700

    no message

--- application/views/dosen/beranda.php
+++ application/views/dosen/beranda.php
@@ -1,14 +1,30 @@
-<html>
-  <head>
-    <title>Halaman Dosen</title>
-  </head>
-  <body>
+
+<div class="main">
+  
+  <div class="main-inner">
+
+      <div class="container">
+        
+       <div class="row">
+          
+          <div class="span12">
+        
+          <div class="info-box">
+
+            <div class="row-fluid stats-box">
       <center>
         <h1>Silahkan replace file ini jika sudah punya desain</h1><br />
         <h2>SELAMAT DATANG ANDA TELAH BERHASIL LOGIN SEBAGAI Dosen</h2>
         <h3> Username Anda Adalah <?php echo $this->session->userdata('username'); ?></h3><br /><br />
         <a href="<?php echo site_url('authentication/auth/logout'); ?>">Keluar</a>
       </center>
+            </div>
+         </div>
+         </div>
+        </div>
+      </div>
+  </div>
+</div>    
     </form>
   </body>
 </html>
--- application/views/dosen/header.php
+++ application/views/dosen/header.php
@@ -34,10 +34,10 @@
                             </ul>
                         </li>
                         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
-                            class="icon-user"></i> EGrappler.com <b class="caret"></b></a>
+                            class="icon-user"></i> <?php echo $this->session->userdata('username'); ?> <b class="caret"></b></a>
                             <ul class="dropdown-menu">
                      
-                                <li><a href="javascript:;">Logout</a></li>
+                                <li><a href="<?php echo site_url('authentication/auth/logout'); ?>">Logout</a></li>
                             </ul>
                         </li>
                     </ul>
