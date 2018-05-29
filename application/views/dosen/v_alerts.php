5032103fcddb64ae47ef5933cc4cc2f4c5277424
VIRA-MUFR\USER <virananda59@gmail.com>
2018-05-29 21:08:37 +0700

    Input Judul Dosen = DONE

---
+++ application/views/dosen/v_alerts.php
@@ -0,0 +1,7 @@
+
+	<div class="alert alert-success">
+		<button type="button" class="close" data-dismiss="alert">&times;</button>
+			<strong>Sukses!</strong>	Usulan Judul Tugas Akhir Telah Diterima
+	</div>
+  </fieldset>
+     </form>
