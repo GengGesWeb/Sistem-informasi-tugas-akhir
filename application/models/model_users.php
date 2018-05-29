92b15b5da0081c53a49f28543bd9176c11638a44
Dimas Fatah Hilla <dimasfatah2@gmail.com>
2018-05-29 15:59:13 +0700

    no message

--- application/models/model_users.php
+++ application/models/model_users.php
@@ -23,12 +23,7 @@
       return $query;
     }
 
-    public function datadosen($id_user)
-    {
-      $this->db->where('id_user',$id_user);
-      $query = $this->db->get($this->table_dosen)->row();
-      return $query;
-    }
+   
 
 
 
