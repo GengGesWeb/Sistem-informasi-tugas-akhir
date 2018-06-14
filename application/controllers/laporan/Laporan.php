576334a6f444d3dfbaa9c7c6d5c94ee29e00eac5
donny pratama yunior <donnypratama4@gmail.com>
2018-06-14 15:51:56 +0700

    file fpdf dan tambah script di model_koordinator

---
+++ application/controllers/laporan/Laporan.php
@@ -0,0 +1,119 @@
+<?php
+	Class Laporan extends CI_Controller{
+		function __construct() {
+        parent::__construct();
+        $this->load->library('pdf');
+		$this->load->model('Model_koordinator');
+		}
+		
+		function kartu_bimbingan(){
+			
+		}
+		function usulan(){
+			header("Content-type:application/vnd.ms-excel");
+			header("Content-disposition:attachment; filename=daftar_usulan_judul.xls");
+			$data = $this->Model_koordinator->data_usulan();
+			echo"<table align='center' cellspacing='0px' border='1px'>
+			<tr>
+			<th>No</th>
+			<th>NIM</th>
+			<th>Nama</th>
+			<th>Prodi - Golongan</th>
+			<th>Usulan Judul</th>
+			<th>Deskripsi</th>
+			<th>Kategori</th>
+			<th>Dosen Pembimbing</th>
+			</tr>
+			";
+			$a=1;
+			foreach($data as $row){
+				echo"
+					<tr>
+					<td>$a</td>
+					<td>$row->nim</td>
+					<td>$row->nama</td>
+					<td>$row->prodi - $row->golongan</td>
+					<td>$row->judul</td>
+					<td>$row->deskripsi</td>
+					<td>$row->kategori</td>
+					<td>$row->pembimbing</td>
+					</tr>
+				";
+				$a++;
+			}
+			echo"</table>";
+		}
+		function usulan_fik(){
+			header("Content-type:application/vnd.ms-excel");
+			header("Content-disposition:attachment; filename=daftar_usulan_judul_fik.xls");
+			$data = $this->Model_koordinator->data_usulan_fik();
+			echo"<table align='center' cellspacing='0px' border='1px'>
+			<tr>
+			<th>No</th>
+			<th>NIM</th>
+			<th>Nama</th>
+			<th>Prodi - Golongan</th>
+			<th>Usulan Judul</th>
+			<th>Deskripsi</th>
+			<th>Kategori</th>
+			<th>Saran</th>
+			<th>Dosen Pembimbing</th>
+			</tr>
+			";
+			$a=1;
+			foreach($data as $row){
+				echo"
+					<tr>
+					<td>$a</td>
+					<td>$row->nim</td>
+					<td>$row->nama</td>
+					<td>$row->prodi - $row->golongan</td>
+					<td>$row->judul</td>
+					<td>$row->deskripsi</td>
+					<td>$row->kategori</td>
+					<td>$row->saran</td>
+					<td>$row->pembimbing</td>
+					</tr>
+				";
+				$a++;
+			}
+			echo"</table>";
+		}
+		function kartu(){
+		$a = $this->session->userdata('username');
+		//echo $a;
+		$data = $this->Model_koordinator->pembimbing_fik($a);
+		//echo $data[0]->nama;
+		$pdf = new FPDF('p','mm','A4');
+		// membuat halaman baru
+        $pdf->AddPage();
+        // setting jenis font yang akan digunakan
+        $pdf->SetFont('Arial','B',14);
+		// mencetak string 
+        $pdf->Cell(190,7,'Kartu Bimbingan',0,1,'C');
+		$pdf->SetFont('Arial','',12);
+		$pdf->Write(10,'
+		NIM : '.$data[0]->nim.'
+		Nama : '.$data[0]->nama.'
+		Prodi : '.$data[0]->prodi.'
+		Golongan : '.$data[0]->golongan.'
+		Pembimbing : '.$data[0]->pembimbing.'');
+		$pdf->Cell(19,6,'',0,1);
+		$pdf->Cell(19,6,'',0,1);
+		$pdf->Cell(10,6,' NO',1,0);
+		$pdf->Cell(29,6,'     Tanggal',1,0);
+		$pdf->Cell(119,6,'                                          Kegiatan',1,0);
+		$pdf->Cell(29,6,'        TTD',1,1);
+		for($w=1;$w<=30;$w++){
+		$pdf->Cell(10,6,'',1,0);
+		$pdf->Cell(29,6,'',1,0);
+		$pdf->Cell(119,6,'',1,0);
+		$pdf->Cell(29,6,'',1,1);
+		}
+		
+		$pdf->Output();
+		}
+		
+	
+	}
+?>
--- application/models/Model_koordinator.php
+++ application/models/Model_koordinator.php
@@ -16,6 +16,18 @@ class Model_koordinator extends CI_Model {
 		$query = $this->db->query("SELECT * FROM tb_dosen WHERE id_dosen = '$id'");
 		return $query->result_array();
 	}
+	function data_usulan(){
+		$query = $this->db->query("SELECT tb_mahasiswa.nim,tb_mahasiswa.nama,tb_mahasiswa.prodi,tb_mahasiswa.golongan , tb_judul_usulan.judul,tb_judul_usulan.deskripsi,tb_judul_usulan.kategori,tb_dosen.nama as pembimbing FROM `tb_judul_usulan` inner join tb_mahasiswa on tb_mahasiswa.nim = tb_judul_usulan.nim inner join tb_dosen on tb_dosen.id_dosen=tb_judul_usulan.id_dosen_pembimbing ORDER BY `tb_judul_usulan`.`id_judul_usulan` ASC");
+		return $query->result();
+	}
+	function data_usulan_fik(){
+		$query = $this->db->query("SELECT tb_mahasiswa.nim,tb_mahasiswa.nama,tb_mahasiswa.prodi,tb_mahasiswa.golongan , tb_final.judul,tb_final.deskripsi,tb_final.kategori,tb_final.saran,tb_dosen.nama as pembimbing FROM `tb_final` inner join tb_mahasiswa on tb_mahasiswa.nim = tb_final.nim inner join tb_dosen on tb_dosen.id_dosen=tb_final.id_dosen ORDER BY `tb_final`.`id_final` ASC");
+		return $query->result();
+	}
+	function pembimbing_fik($id){
+		$query = $this->db->query("SELECT tb_pembimbing_fix.nim,tb_mahasiswa.nama,tb_mahasiswa.prodi,tb_mahasiswa.golongan,tb_dosen.nama as pembimbing FROM tb_pembimbing_fix inner join tb_mahasiswa on tb_pembimbing_fix.nim = tb_mahasiswa.nim inner join tb_dosen on tb_pembimbing_fix.id_dosen = tb_dosen.id_dosen where tb_pembimbing_fix.nim ='$id'");
+		return $query->result();
+	}
 }
 
 ?>
